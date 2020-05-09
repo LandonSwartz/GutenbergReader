8338e88185785aa69a71a616d5092cb8af94c58b
LandonSwartz <lgsm2n@mail.missouri.edu>
2020-05-08 20:08:48 -0500

    Added return

--- main.cpp
+++ main.cpp
@@ -5,7 +5,22 @@
 #include<pthread.h>
 #include<iostream>
 
+//example command line
+//	./app filename
+
 int main(int argc, char *argv[])
 {
+	File f; //file class
 
+	if(argc < 2)
+	{
+		//help message about how to use program
+	}
+	else
+	{
+		
+	}
+
+	return 0;
+	
 }
